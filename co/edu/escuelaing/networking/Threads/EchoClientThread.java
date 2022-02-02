050afd105bdd3b4c4b23b08b0cd83476f8708ce8
Rincon10 <ivan.rincon-s@mail.escuelaing.edu.co>
2022-02-02 15:25:21 -0500

    EchoClientThread

---
+++ co/edu/escuelaing/networking/Threads/EchoClientThread.java
@@ -0,0 +1,60 @@
+package co.edu.escuelaing.networking.Threads;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class EchoClientThread extends Thread{
+    protected Socket echoSocket;
+    protected PrintWriter out = null;
+    protected BufferedReader in = null;
+
+    public EchoClientThread(Socket clientSocket) {
+        this.echoSocket = clientSocket;
+    }
+
+    public void closeConnection(BufferedReader stdIn) throws IOException {
+        out.close();
+        in.close();
+        stdIn.close();
+        echoSocket.close();
+    }
+
+    public void startConnection() throws IOException {
+
+        try {
+            echoSocket = new Socket("127.0.0.1", 35000);
+            out = new PrintWriter(echoSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don’t know about host!.");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn’t get I/O for " + "the connection to: localhost.");
+            System.exit(1);
+        }
+        BufferedReader stdIn = new BufferedReader(
+                new InputStreamReader(System.in));
+        String userInput;
+        while ((userInput = stdIn.readLine()) != null) {
+            out.println(userInput);
+            System.out.println("echo: " + in.readLine());
+        }
+        closeConnection( stdIn );
+
+
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            startConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
