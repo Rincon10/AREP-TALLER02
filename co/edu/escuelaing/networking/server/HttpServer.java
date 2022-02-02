51acaed01c813b47f97ac55cfbba88fb4e002dfa
Rincon10 <ivan.rincon-s@mail.escuelaing.edu.co>
2022-02-02 15:05:51 -0500

    HttpServer one petition at time

--- co/edu/escuelaing/networking/server/HttpServer.java
+++ co/edu/escuelaing/networking/server/HttpServer.java
@@ -7,7 +7,7 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class EchoServer {
+public class HttpServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         try {
@@ -18,23 +18,41 @@ public class EchoServer {
         }
         Socket clientSocket = null;
         try {
-            clientSocket = serverSocket.accept(); // El se queda esperando aqui hasta que le llega una conexion, este programa solo lee una peticion
+            System.out.println("Listo para recibir ...");
+            clientSocket = serverSocket.accept();
         } catch (IOException e) {
             System.err.println("Accept failed.");
             System.exit(1);
         }
+
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(
                         clientSocket.getInputStream()));
         String inputLine, outputLine;
+
         while ((inputLine = in.readLine()) != null) {
-            System.out.println("Mensaje:" + inputLine);
-            outputLine = "Respuesta desde el server " + inputLine;
-            out.println(outputLine);
-            if (outputLine.equals("Respuestas: Bye."))
+            System.out.println("Received: " + inputLine);
+            if (!in.ready()) {
                 break;
+            }
         }
+        outputLine = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>"
+                + "<html>"
+                + "<head>"
+                + "<meta charset=\"UTF-8\">"
+                + "<title>Title of the document</title>\n"
+                + "</head>"
+                + "<body>"
+                + "My Web Site HOLIUIIIIIIII"
+                + "</body>"
+                + "</html>" ;
+        System.out.println(outputLine);
+        out.println(outputLine);
+
         out.close();
         in.close();
         clientSocket.close();
